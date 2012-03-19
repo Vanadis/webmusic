5b5201a221cdb90e2511cc97a48e41f8852c7f7e
lawl <root@s3cr3t.de>
2012-03-19 20:14:28 +0100

    add design

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-r<?php
+<?php
     require_once ('config.php');
     require_once ('lib/musicindex.php');
     require_once ('lib/id3.php');
+    include 'templates/header.php';
 
+    
+    include 'templates/footer.php';
 ?>
